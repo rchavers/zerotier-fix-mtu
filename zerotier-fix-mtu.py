6a5008ae763f4e34d1cc7a368c0e840d8f297941
Robert Chavers <56808029+rchavers@users.noreply.github.com>
2022-03-23 15:54:54 -0400

    Update zerotier-fix-mtu.py

--- zerotier-fix-mtu.py
+++ zerotier-fix-mtu.py
@@ -2,12 +2,13 @@
 
 """
 rac: 2022-03-21 fix-zerotier-mtu.py
-this program tries to set a better mtu for the zerotier interface
+This program tries to set a better mtu for the zerotier interface
 """
 
 import sys
 import subprocess
 
+DEFAULT_MTU='2800'
 
 # check that we were given at least on argument
 # 0=program name, 1=ip_address
@@ -30,14 +31,13 @@ if not interface.lower().startswith('zt'):
     sys.exit(1)
 
 
-# (re)set the mtu to zt default of 2800
-mtu = '2800'
-print ('setting the mtu on interface %s to %s' %(interface, mtu))
-ip_link_set = 'ip link set %s mtu %s' %(interface, mtu)
+# (re)set the mtu to zt default_mtu
+print ('setting the mtu on interface %s to %s' %(interface, DEFAULT_MTU=))
+ip_link_set = 'ip link set %s mtu %s' %(interface, DEFAULT_MTU=)
 subprocess.getoutput(ip_link_set)
 
 
-# find the optimal MTU using probe_mtu from ofunctions.network
+# find the optimal MTU using ping -M do
 print ('please wait, checking for optimal MTU to %s via %s' %(ip_dst, interface))
 cmd="""bash -c "for i in {900..1500..2}; do ping -M do -c 1 %s -s \$i 2>&1 | grep -q '0 received' && break; done; echo \$((\$i - 2));" """ %ip_dst
 mtu=subprocess.getoutput(cmd)
